74878a6edbaf93cb05584881fb9efe96f52a6700
MarlonVictor <cmarlonvictor11@gmail.com>
2022-06-20 23:00:17 -0300

    Configuring graphql query with apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,34 @@
+import { gql, useQuery } from "@apollo/client"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 function App() {
+  const { data } =  useQuery(GET_LESSONS_QUERY)
+  
   return (
-    <h1 className="text-3xl font-semibold">Hello Word</h1>
+    <>
+      <h1 className="text-3xl font-semibold">Hello Word</h1>
+
+      <br />
+
+      <ul>
+        {data?.lessons.map((lesson: Lesson) => {
+          return <li key={lesson.id}>{lesson.title}</li>
+        })}
+      </ul>
+    </>
   )
 }
 
